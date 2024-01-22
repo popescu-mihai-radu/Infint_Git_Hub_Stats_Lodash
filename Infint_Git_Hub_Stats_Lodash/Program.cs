9596e6dd4e13356150fcfa68f79c00acfa7ae088
Mihai-Radu Popescu <mihai-radu.popescu@pwc.lu>
2024-01-22 08:27:11 +0100

    V2

--- Infint_Git_Hub_Stats_Lodash/Program.cs
+++ Infint_Git_Hub_Stats_Lodash/Program.cs
@@ -1,26 +1,35 @@
-﻿using System;
+﻿////  -> Sample programm for GitHub statics using diffrent programming approches for performance demo
+////  -> This program can also be further improve to minimum exceution time by taking advantage of the Commit Diffrance in Git 
+////    and so to persist the last execution statistics and to apply the computing only on the delta (diffrance from the last commit)
+////  -> Also could be used as a scheduled service and if used in a WebService better to keep using it as associated simple console app (if scheduald) cause 
+////  not needed to consume webserices resources
+//// Author Radu Mihai POPESCU  (popescu.mihai.radu@gmail.com)
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Octokit;
+using System.Collections.Concurrent;
+using System.Threading;
 
 class Program
 {
     static async Task Main(string[] args)
     {
-
-        // Replace 'YourAccessToken' with your actual GitHub personal access token
-        var github = new GitHubClient(new ProductHeaderValue("YourAppName"))
+        Console.Clear();
+        // Replace 'token' with your actual GitHub personal access token in case of not working 
+        var github = new GitHubClient(new ProductHeaderValue("Infint_Git_Hub_Stats_Lodash"))
         {
-            Credentials = new Credentials("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")
+            Credentials = new Credentials("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")
         };
 
         // Replace 'lodash' with the desired repository owner and 'lodash' with the repository name
         var repositoryOwner = "lodash";
         var repositoryName = "lodash";
-        var baseUrl = "https://raw.githubusercontent.com/" + repositoryOwner + "/" + repositoryName+ "/main/";
+        var baseUrl = "https://raw.githubusercontent.com/" + repositoryOwner + "/" + repositoryName + "/main/";
 
         try
         {
@@ -33,17 +42,27 @@ class Program
             Console.WriteLine($"Contributors Count: {contributors.Count}");
 
             // Get JavaScript/TypeScript files on all sublfolders 
-            var jsTsFiles2 = GetJsTsFilesTree(github, repositoryOwner, repositoryName,3);
-            Console.WriteLine($"Total JavaScript/TypeScript files Count: {jsTsFiles2.Count}");
-            var letterCount2 = GetLetterFrequency(jsTsFiles2, baseUrl);
-            OutputLetterFrequency(letterCount2.Result);
+            var jsTsFilesAllSubfoldersFromRoot = GetJsTsFilesTree(github, repositoryOwner, repositoryName, 10);
+            Console.WriteLine($"Total JavaScript/TypeScript files Count: {jsTsFilesAllSubfoldersFromRoot.Count}");
+            var letterCountForThreading = GetLetterFrequency(jsTsFilesAllSubfoldersFromRoot, baseUrl);
+            Console.WriteLine();
+            Console.WriteLine($"*** Multithread computing ***");
+            OutputLetterFrequency(letterCountForThreading.Result);
+
+            // Parallel call for performances 
+            var letterCountForParallel = GetLetterFrequencyConcurent(jsTsFilesAllSubfoldersFromRoot, baseUrl);
+            Console.WriteLine();
+            Console.WriteLine($"*** Parallel computing ***");
+            OutputLetterFrequencyConcurent(letterCountForParallel);
 
             // Get JavaScript/TypeScript files on root folder 
-            var jsTsFiles = await GetJsTsFiles(github, repositoryOwner, repositoryName);
-            Console.WriteLine($"Total JavaScript/TypeScript files Count: {jsTsFiles.Count}");
+            var jsTsFilesRootOnly = await GetJsTsFiles(github, repositoryOwner, repositoryName);
+            Console.WriteLine();
+            Console.WriteLine($"*** Only Root Level ***");
+            Console.WriteLine($"Total JavaScript/TypeScript files Count: {jsTsFilesRootOnly.Count}");
 
             // Output letter frequency in decreasing order
-            var letterCount = GetLetterFrequency(jsTsFiles);
+            var letterCount = GetLetterFrequency(jsTsFilesRootOnly);
             OutputLetterFrequency(letterCount.Result);
         }
         catch (NotFoundException)
@@ -54,7 +73,7 @@ class Program
         {
             Console.WriteLine($"An error occurred: {ex.Message}");
         }
-        Console.ReadLine();
+        _ = Console.ReadLine();
     }
 
     static List<TreeItem> GetJsTsFilesTree(GitHubClient github, string owner, string repo, int maxNoOfRecords)
@@ -102,6 +121,42 @@ class Program
             }
         }
 
+        return letterCount;
+    }
+
+    static ConcurrentDictionary<char, int> GetLetterFrequencyConcurent(List<TreeItem> files, string baseUrl)
+    {
+        var letterCount = new ConcurrentDictionary<char, int>();
+        ParallelOptions parallelOptions = new()
+        {
+            MaxDegreeOfParallelism = 4
+        };
+        try
+        {
+            Parallel.ForEach(files, parallelOptions, file =>
+            {
+                var fileContent =  GetFileContent(baseUrl + file.Path).Result;
+                foreach (char letter in fileContent)
+                {
+                    if (char.IsLetter(letter))
+                    {
+                        var lowercaseLetter = char.ToLower(letter);
+                        if (letterCount.ContainsKey(lowercaseLetter))
+                        {
+                            letterCount[lowercaseLetter]++;
+                        }
+                        else
+                        {
+                            letterCount[lowercaseLetter] = 1;
+                        }
+                    }
+                }
+            });
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Exception : {ex.Message}");
+        }
         return letterCount;
     }
 
@@ -157,5 +212,15 @@ class Program
             Console.WriteLine($"Letter: {entry.Key}, Occurrences: {entry.Value}");
         }
     }
+
+    static void OutputLetterFrequencyConcurent(ConcurrentDictionary<char, int> letterCount)
+    {
+        var sortedLetterCount = letterCount.OrderByDescending(pair => pair.Value);
+        foreach (var entry in sortedLetterCount)
+        {
+            Console.WriteLine($"Letter: {entry.Key}, Occurrences: {entry.Value}");
+        }
+    }
+
 }
 
